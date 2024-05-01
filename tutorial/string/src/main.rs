4283c82c919f7575d9deb519d26c72731a3fe3cf
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-05-01 17:31:54 +0530

    update memory management concept

--- tutorial/string/src/main.rs
+++ tutorial/string/src/main.rs
@@ -2,7 +2,10 @@ fn main() {
     // println!("Hello, world!");
     // let x = "rajesh shedolkar";
     let s = String::from("Rajesh Shedolkar");
-    let y = s.chars().nth(78);
+    let y = s.chars().nth(7);
+
+    // This works if not out of index
+    // print!("by unwrapping char: {}\n", y.unwrap());
 
     match y {
         Some(z) => {
@@ -13,4 +16,51 @@ fn main() {
         }
     }
     // print!("{}", y);
+
+    let is_even = false;
+    if is_even{
+        print!("even number");
+    } else if !is_even{
+        print!("Odd number");
+    }
+    
+
+    for i in 0..10{
+        print!("\n{}", i);
+    }
+    
+
+    // iterate over arrays, map, strings 
+    let str = String::from("Hello World Rust");
+    print!("first word: {}", first_word(str));
+
+
+    let num = String::from("Rajesh Shedolkar");
+    onwner_ship(num);
+    print!("num after function call: {}", num);
+    print!("\n");
+}
+
+
+fn onwner_ship(num: String){
+    print!( "num in ownership function: {}", num);
+}
+
+
+
+fn first_word(str: String) -> String{
+    let mut ans = String::from("");
+    for c in str.chars() {
+        print!("\n{}", c);
+        ans.push(c);
+        if c == ' '{
+            break;
+        }
+    }
+
+    for (i, c) in str.chars().enumerate(){
+        print!("({}, {}), ", i, c);
+    }
+
+    return ans;
 }
